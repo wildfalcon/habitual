0d034bffcf30c4febb6a10ae0a335868d0fcb249
Wildfalcon <laurie@wildfalcon.com>
2010-12-08 12:58:56 +0000

    Added habbit resource

---
+++ db/migrate/20101208125802_create_habits.rb
@@ -0,0 +1,16 @@
+class CreateHabits < ActiveRecord::Migration
+  def self.up
+    create_table :habits do |t|
+      t.string :name
+      t.date :start_date
+      t.date :end_date
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :habits
+  end
+end
---
+++ spec/models/habit_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Habit do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
