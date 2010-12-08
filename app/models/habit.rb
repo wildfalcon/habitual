870be50c969a20cf8f10c56ec91cda785679cbfc
Wildfalcon <laurie@wildfalcon.com>
2010-12-08 16:36:59 +0000

    Habits create start date on creation

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,10 @@
 class Habit < ActiveRecord::Base
+  before_create :set_start_date
+  
+  private
+  
+  def set_start_date
+    self.start_date = Time.now.to_date
+  end
+
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Habit do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "on create" do
+    it "should set start_date to today" do
+      habit = Habit.create!
+      habit.start_date.should == Time.now.to_date
+    end
+  end
 end
