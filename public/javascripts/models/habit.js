11cb6e1cbda019415e995e5156043c0426f601e2
Wildfalcon <laurie@wildfalcon.com>
2010-12-08 16:37:27 +0000

    Nice Display of Calendar

--- public/javascripts/models/habit.js
+++ public/javascripts/models/habit.js
@@ -1,11 +1,18 @@
 var Habit = Model("habit", {
   persistence: Model.REST("/habits")
-})
+}, {
+  startDate: function(){
+    return Date.parse(this.attr("start_date"))
+  },
+  allDays: function(){
+    var days = []
+    for(var i = 0; i<=29; i++){
+      days.push(this.startDate().addDays(i))
+    }
+    return days
+  }
+  
+});
 
 
-// wait for the document to load
-$(function() {
-  Habit.load(function() {
-    Habit.trigger('habits.loaded');
-  })
-})
+
--- public/javascripts/plugins/habit_list.js
+++ public/javascripts/plugins/habit_list.js
@@ -1,11 +1,35 @@
 (function($){
 
   var HabitRow = function(habit){
-    var self = this
+    var self = this;
+    self.habit = habit;
     self.$elem = $("<div>").addClass("habit");
     $('<h2>').html(habit.attr("name")).appendTo(self.$elem);
+    self.$elem.click(function(){self.showCalendar();});
     return self.$elem;
-  }
+  };
+  HabitRow.prototype = {
+    showCalendar: function(){
+      this.hideAllCalendars();
+      if (this.$habitCalendar == null){
+        this.$habitCalendar = this.createCalendar();
+      }
+      this.$habitCalendar.hide();
+      this.$elem.append(this.$habitCalendar);
+      this.$habitCalendar.slideDown();
+    },
+    hideAllCalendars: function(){
+      $('.habit .calendar').slideUp();
+    },
+    createCalendar: function(){
+      var cal = $('<div>').addClass("calendar");
+      $.each(this.habit.allDays(), function(index, day){
+        var day = $("<div>").addClass("day").html(day.getDate()+"/"+(day.getMonth()+1));
+        day.appendTo(cal);
+      });
+      return cal;
+    }
+  };
 
   var HabitList = function($elem){
     var self = this;
@@ -15,7 +39,7 @@
       $.each(Habit.all(), function(index, habit){
         self.addHabitRow(habit);
       });
-    })
+    });
   };
   HabitList.prototype = {
     addHabitRow: function(habit){
