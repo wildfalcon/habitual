1a5187ae5aaa45dbb5ed1030aff2ef4d82d96523
Wildfalcon <laurie@wildfalcon.com>
2010-12-08 13:58:55 +0000

    Get habits in JSModel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var Habit = Model("habit", {
+  persistence: Model.REST("/habits")
+})
+
+
+// wait for the document to load
+$(function() {
+  Habit.load(function() {
+    console.log("loaded")
+  })
+})
