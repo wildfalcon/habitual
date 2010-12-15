3e7b4d5c2158ee11a21428e9130be05d34d68173
Wildfalcon <laurie@wildfalcon.com>
2010-12-15 12:32:25 +0000

    Set up user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Habitual::Application.routes.draw do
 
+  devise_for :users
+
   root :to => "home#index"
 
   resources :habits
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101208125802) do
+ActiveRecord::Schema.define(:version => 20101215121753) do
 
   create_table "habits", :force => true do |t|
     t.string   "name"
@@ -20,5 +20,14 @@ ActiveRecord::Schema.define(:version => 20101208125802) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                             :default => "", :null => false
+    t.string   "encrypted_password", :limit => 128, :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
