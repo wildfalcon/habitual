3236e839a74b6828eb441387c5069b3875ab436a
Laurie Young <laurie@wildfalcon.com>
2010-12-27 12:48:46 +0000

    Spike pulling in friends from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,28 @@ class User < ActiveRecord::Base
       "https://graph.facebook.com/#{uid}/picture?type=square"
     end
 
+    def pull_friends_from_facebook
+      uri = URI.parse(URI.escape("https://graph.facebook.com/#{uid}/friends?access_token=#{access_token}"))
+      puts uri.request_uri 
+
+      http_session = Net::HTTP.new(uri.host, uri.port)
+      http_session.use_ssl = true
+      res = http_session.start { |http|
+        http.get(uri.request_uri)
+      }
+      
+      hash = JSON.parse(res.body);
+      if hash.present? && hash["data"].present?
+        friend_ids = hash["data"].inject([]){|a, v| a << v["id"]; a}
+        # puts friends
+        
+        freinds = User.all(:conditions => ["uid in (?)", friend_ids])
+        # puts friends.count
+        
+      end
+      # puts friends.inspect
+    end
+
     private
 
     def pull_profile_from_facebook
@@ -24,4 +46,6 @@ class User < ActiveRecord::Base
       self.attributes = JSON.parse(res.body).slice("name", "email")
       self.save!
     end
+    
+    
 end
