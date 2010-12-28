a52b59b3a4fa9ca98fbb3f15de94c939dc812b79
Laurie Young <laurie@wildfalcon.com>
2010-12-28 00:15:34 +0000

    Users can now be queried for friends with habits

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,32 +4,21 @@ class User < ActiveRecord::Base
   
     has_many :habits
   
+    serialize :friend_ids, Array
+  
     after_create :pull_profile_from_facebook
+    after_create :pull_friend_ids_from_facebook
 
     def profile_url(size = "square")
       "https://graph.facebook.com/#{uid}/picture?type=square"
     end
 
-    def pull_friends_from_facebook
-      uri = URI.parse(URI.escape("https://graph.facebook.com/#{uid}/friends?access_token=#{access_token}"))
-      puts uri.request_uri 
-
-      http_session = Net::HTTP.new(uri.host, uri.port)
-      http_session.use_ssl = true
-      res = http_session.start { |http|
-        http.get(uri.request_uri)
-      }
-      
-      hash = JSON.parse(res.body);
-      if hash.present? && hash["data"].present?
-        friend_ids = hash["data"].inject([]){|a, v| a << v["id"]; a}
-        # puts friends
-        
-        freinds = User.all(:conditions => ["uid in (?)", friend_ids])
-        # puts friends.count
-        
-      end
-      # puts friends.inspect
+    def friends
+      User.where(:uid =>  friend_ids)
+    end
+    
+    def friends_with_habits
+      friends.joins(:habits).group("id").includes(:habits)
     end
 
     private
@@ -47,5 +36,19 @@ class User < ActiveRecord::Base
       self.save!
     end
     
-    
+    def pull_friend_ids_from_facebook
+      uri = URI.parse(URI.escape("https://graph.facebook.com/#{uid}/friends?access_token=#{access_token}"))
+      puts uri.request_uri 
+
+      http_session = Net::HTTP.new(uri.host, uri.port)
+      http_session.use_ssl = true
+      res = http_session.start { |http|
+        http.get(uri.request_uri)
+      }
+      
+      hash = JSON.parse(res.body);
+      if hash.present? && hash["data"].present?
+        self.friend_ids = hash["data"].inject([]){|a, v| a << v["id"]; a}
+      end
+    end
 end
