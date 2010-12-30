87f355cbf46e4616d8eac2cd00c71fe0b4b68c79
Laurie Young <laurie@wildfalcon.com>
2010-12-30 11:28:59 +0000

    Added method to publish to facebook wall

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,22 @@ class User < ActiveRecord::Base
       friends.includes(:habits).select{|u| u.habits.uncompleted.count > 0}
     end
 
+    def post_to_facebook(message)
+      url = URI.parse("https://graph.facebook.com/#{uid}/feed")
+      request = Net::HTTP::Post.new(url.path)
+      request.set_form_data({'access_token'=>self.access_token, 
+        'message'=> message,
+        'link' => "http://habitualapp.com",
+        'caption' => "Making Habits that Stick",
+        'name' => "HabitualApp"
+      })
+
+      http_session = Net::HTTP.new(url.host, url.port)
+      http_session.use_ssl = true
+
+      res = http_session.start {|http| http.request(request) }
+      puts res.body
+    end
 
     def pull_profile_from_facebook
       uri = URI.parse(URI.escape("https://graph.facebook.com/#{uid}?access_token=#{access_token}"))
