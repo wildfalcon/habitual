3d2ba510da8f75346bb197982bfd8406500fb098
Laurie Young <laurie@wildfalcon.com>
2010-12-30 11:51:01 +0000

    Post to facebook on habit create and success. But only in prod env

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -7,6 +7,7 @@ class Habit < ActiveRecord::Base
   scope :uncompleted, where(:completed => false)
 
   before_create :set_start_date
+  before_save :post_start_to_facebook
   before_save :complete_if_done
 
   validate do
@@ -19,12 +20,23 @@ class Habit < ActiveRecord::Base
   private
 
   def set_start_date
-    self.start_date = Time.now.to_date
+    if new_record?
+      self.start_date = Time.now.to_date
+    end
   end
-  
+
+  def post_start_to_facebook
+    if new_record? and user.present?
+      user.post_to_facebook("Started a new habit\n#{name}")
+    end
+  end
+
   def complete_if_done
     if last_completed_date.present? && (last_completed_date - 28.days) > start_date
       self.completed =  true
+      if user.present?
+        user.post_to_facebook("Succesfully managed to: #{name}\n for 30 days in a row")
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,21 +23,25 @@ class User < ActiveRecord::Base
     end
 
     def post_to_facebook(message)
-      url = URI.parse("https://graph.facebook.com/#{uid}/feed")
-      request = Net::HTTP::Post.new(url.path)
-      request.set_form_data({'access_token'=>self.access_token, 
-        'message'=> message,
-        'link' => "http://habitualapp.com",
-        'caption' => "Making Habits that Stick",
-        'name' => "HabitualApp"
-      })
+      if Rails.env == "production"
+        url = URI.parse("https://graph.facebook.com/#{uid}/feed")
+        request = Net::HTTP::Post.new(url.path)
+        request.set_form_data({'access_token'=>self.access_token, 
+          'message'=> message,
+          'link' => "http://habitualapp.com",
+          'caption' => "Making Habits that Stick",
+          'name' => "HabitualApp"
+          })
 
-      http_session = Net::HTTP.new(url.host, url.port)
-      http_session.use_ssl = true
+          http_session = Net::HTTP.new(url.host, url.port)
+          http_session.use_ssl = true
 
-      res = http_session.start {|http| http.request(request) }
-      puts res.body
-    end
+          res = http_session.start {|http| http.request(request) }
+          puts res.body
+        else
+          puts "Would have posted '#{message}' to facebook if I was running in production"
+        end
+      end
 
     def pull_profile_from_facebook
       uri = URI.parse(URI.escape("https://graph.facebook.com/#{uid}?access_token=#{access_token}"))
