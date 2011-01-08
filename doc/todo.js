24908ee8544ef1bcdca55f931385ae507098cb89
Laurie Young <laurie@wildfalcon.com>
2011-01-08 17:19:18 +0000

    Fixed clicktail code and updated todo

--- doc/todo.js
+++ doc/todo.js
@@ -1,7 +1,8 @@
-last logged in at count
-email opt-in
-progress bar on friends habits
 option to not post habits on facebook
-habit page, with facebook like and comments
-add pop up to initial experience
+log-in without Facebook
+last logged in at count
 progress indicators when ajax saving
+progress bar on friends habits
+add email opt-in pop up as initial experience
+habit page, with facebook like and comments
+
