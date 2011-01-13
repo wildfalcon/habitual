93874a733c7ff8073a107f673bef67efe0c60b13
Wildfalcon <laurie@wildfalcon.com>
2011-01-13 17:52:39 +0000

    updated todo

--- doc/todo.js
+++
@@ -1,8 +0,0 @@
-option to not post habits on facebook
-log-in without Facebook
-last logged in at count
-progress indicators when ajax saving
-progress bar on friends habits
-add email opt-in pop up as initial experience
-habit page, with facebook like and comments
-
